a268dff3e38663f5cfe860f70eb77df7b79f74d3
Guilherme Oliveira <htoguilherme@gmail.com>
2021-07-19 11:51:51 +1200

    Fix build

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ export default function Home() {
 		<div className="flex items-center justify-center flex-col h-screen">
 			<div>Welcome. This is a work in progress.</div>
 			<div>
-				While it's not done, please, feel free to check my old - and outdated - portfolio.
+				While it&apos;s not done, please, feel free to check my old - and outdated -
+				portfolio.
 			</div>
 			<a
 				className="border-2 border-gray-400 mt-4 p-2 rounded-lg"
