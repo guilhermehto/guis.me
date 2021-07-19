6b5ffebb058f16e5e3527a64423f32c888e52321
Guilherme Oliveira <htoguilherme@gmail.com>
2021-07-19 14:04:52 +1200

    Update headline

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<div className="flex items-center justify-center flex-col h-screen">
-			<div>Welcome. This is a work in progress.</div>
+			<div>Welcome. This is still a work in progress.</div>
 			<div>
 				While it&apos;s not done, please, feel free to check my old - and outdated -
 				portfolio.
