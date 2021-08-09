902fc9102f5cc050e401e1bfe8e0439d8be78dfb
Guilherme Oliveira <htoguilherme@gmail.com>
2021-08-09 21:57:41 +1200

    🖊 Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,21 @@ module.exports = {
 	darkMode: false, // or 'media' or 'class'
 	mode: 'jit',
 	theme: {
+		extend: {
+			fontSize: {
+				'20xl': '24rem',
+			},
+		},
 		colors: {
 			primary: '#9A8C98',
 			secondary: '#4A4E69',
 			ternary: '#22223B',
 			white: '#ffffff',
+		},
+
+		fontFamily: {
+			display: ['Staatliches'],
+			body: ['"Open Sans"'],
 		},
 	},
 	variants: {
