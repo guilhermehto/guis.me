a8bbbe37796dccac02543832646ed999f65b37d1
Guilherme Oliveira <htoguilherme@gmail.com>
2021-08-09 21:19:02 +1200

    🎨 Add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
 	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	darkMode: false, // or 'media' or 'class'
+	mode: 'jit',
 	theme: {
-		extend: {},
+		colors: {
+			primary: '#9A8C98',
+			secondary: '#4A4E69',
+			ternary: '#22223B',
+			white: '#ffffff',
+		},
 	},
 	variants: {
 		extend: {},
